f8f892de5eec293c64a1f55505d42b63072192c1
James Gaboardi <jgaboardi@gmail.com>
2020-04-29 17:21:22 -0400

    base py file as geocrosswalk

---
+++ nhgisxwalk/geocrosswalk.py
@@ -0,0 +1,65 @@
+"""IPUMS/NHGIS Census Crosswalk and Atom Generator
+"""
+
+
+from .identifiers import str_types, get_context, code_cols
+from .identifiers import bgp_id, trt_id, id_from
+from .codes import code_desc_1990_blk, desc_code_1990_blk
+
+
+__all__ = ["GeoCrossWalk"]
+
+
+# stuff from data prep as class here...
+
+
+class GeoCrossWalk:
+    """
+    
+    """
+
+    def __init__(self,):
+        """
+        
+        
+        
+        """
+
+        pass
+
+        # do step one
+
+        # do step two
+
+        # method for atom to returns dataframe
+
+    def step_one():
+        """
+        """
+
+        pass
+
+    def step_two():
+        """
+        """
+
+        pass
+
+
+# To Do ----- v0.0.1
+#
+# ID generator: -- blk_id, bkg_id, cty_id...
+#
+# API structure
+#   -- only import the class here
+
+# switch class!
+
+# base.py
+
+# from .base import GeoXwalk
+
+
+# atom calculator as a method in GeoXwalk
+
+#
