576a31f03c959e6b77176ffb2e6f7ea4659ba80e
James Gaboardi <jgaboardi@gmail.com>
2020-04-29 18:13:39 -0400

    updating init

--- nhgisxwalk/__init__.py
+++ nhgisxwalk/__init__.py
@@ -1,13 +1,35 @@
-""" IPUMS/NHGIS Census Crosswalk and Atom Generator
+__version__ = "0.0.1"
 """
-__version__ = "0.0.0"
+:mod:`nhgisxwalk` --- IPUMS/NHGIS Census Crosswalk and Atom Generator
+====================================================================
+"""
 __author__ = "James Gaboardi <jgaboardi@gmail.com>"
 __date__ = "2020-04"
 
 
-from .atom_calculator import calculate_atoms
-from .identifiers import str_types, get_context, code_cols
-from .identifiers import bgp_id, trt_id, id_from
-from .codes import code_desc_1990_blk, desc_code_1990_blk
+from .geocrosswalk import GeoCrossWalk
 
-# blk_id, bkg_id, cty_id...
+
+# from .atom_calculator import calculate_atoms
+# from .identifiers import str_types, get_context, code_cols
+# from .identifiers import bgp_id, trt_id, id_from
+# from .codes import code_desc_1990_blk, desc_code_1990_blk
+
+
+# To Do ----- v0.0.1
+#
+# ID generator: -- blk_id, bkg_id, cty_id...
+#
+# API structure
+#   -- only import the class here
+
+# switch class!
+
+# base.py
+
+# from .base import GeoXwalk
+
+
+# atom calculator as a method in GeoXwalk
+
+#
