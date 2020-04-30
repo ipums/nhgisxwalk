2c863c2efc9f65c80fe7987e799dffea4b1ce2ab
James Gaboardi <jgaboardi@gmail.com>
2020-04-30 09:28:51 -0400

    updated tests (no real tests)

--- nhgisxwalk/tests/test_nhgisxwalk.py
+++ nhgisxwalk/tests/test_nhgisxwalk.py
@@ -4,6 +4,8 @@
 import unittest
 import numpy
 
+import nhgisxwalk
+
 
 class TestGenericName(unittest.TestCase):
     def setUp(self):
@@ -19,36 +21,31 @@ class TestGenericName(unittest.TestCase):
         known = "thing"
         observed = "thing"
         self.assertEqual(known, observed)
-    
-    
+
     def test_generic_function_assertAlmostEqual(self):
         # testing approximate equality
         known = 0.01
         observed = 0.0100000001
         self.assertAlmostEqual(known, observed, places=3)
-    
+
     def test_generic_function_assertIn(self):
         # testing membership
         known = "thing"
         observed = ["thing", "stuff"]
         self.assertIn(known, observed)
-    
+
     def test_network_failures_assertRaises(self):
         # testing for triggered errors
         with self.assertRaises(TypeError):
             self.thing_that_should_raise_TypeError()
-    
-    
+
     def test_generic_function_numpy_array_equality(self):
         # testing membership
-        known = "thing"
-        observed = ["thing", "stuff"]
-        
+        known = numpy.array([0.0])
+        observed = numpy.array([0.0])
+
         # all close
         numpy.testing.assert_allclose(known, observed)
-        # null?
-        numpy.testing.assert_array_almost_equal_nulp(known, observed)
-
 
 
 if __name__ == "__main__":
