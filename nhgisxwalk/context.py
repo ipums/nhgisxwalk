77879d9d80652fb123a80ab1ca230fe0868e8a56
James Gaboardi <jgaboardi@gmail.com>
2020-04-30 18:30:58 -0400

    add context shell

---
+++ nhgisxwalk/context.py
@@ -0,0 +1,27 @@
+"""
+"""
+
+
+class CodeContext:
+    """
+    """
+
+    def __init__():
+        """
+        """
+
+        pass
+
+
+def fetch_code_context():  ######################## maybe this as a method in GeoXWalk
+    """
+    """
+
+    pass
+
+
+def some_year_context_in_raw_string():
+    """
+    """
+
+    pass
