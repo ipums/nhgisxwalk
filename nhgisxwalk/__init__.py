1c4bae8167aa91bbf594d5622ca68fff5a2618a4
James Gaboardi <jgaboardi@gmail.com>
2020-06-18 13:59:00 -0400

    add new read function to init

--- nhgisxwalk/__init__.py
+++ nhgisxwalk/__init__.py
@@ -9,7 +9,7 @@ __date__ = "2020-04"
 
 
 from .geocrosswalk import GeoCrossWalk
-from .geocrosswalk import xwalk_df_from_csv
+from .geocrosswalk import xwalk_df_to_csv, xwalk_df_from_csv
 from .geocrosswalk import calculate_atoms, round_weights, str_types
 from .geocrosswalk import valid_geo_shorthand, example_crosswalk_data
 
