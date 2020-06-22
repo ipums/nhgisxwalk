69dabb94b7fa39527ec7cf8e11cc1ce0a81088f0
James Gaboardi <jgaboardi@gmail.com>
2020-06-22 13:37:50 -0400

    bumping version

--- nhgisxwalk/__init__.py
+++ nhgisxwalk/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 """
 :mod:`nhgisxwalk` --- IPUMS/NHGIS Census Crosswalk and Atom Generator
 =====================================================================
