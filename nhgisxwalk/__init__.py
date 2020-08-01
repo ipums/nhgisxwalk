82c1597f14b292d5170d088a512a7cdb759dee22
James Gaboardi <jgaboardi@gmail.com>
2020-08-01 17:54:33 -0400

    commit intermediary __init__

--- nhgisxwalk/__init__.py
+++ nhgisxwalk/__init__.py
@@ -8,11 +8,14 @@ __author__ = "James Gaboardi <jgaboardi@gmail.com>"
 __date__ = "2020-04"
 
 
-from .geocrosswalk import GeoCrossWalk, sort_params
+from .geocrosswalk import GeoCrossWalk
 from .geocrosswalk import xwalk_df_to_csv, xwalk_df_from_csv, split_blk_blk_xwalk
 from .geocrosswalk import extract_state, extract_unique_stfips
 from .geocrosswalk import calculate_atoms, round_weights, str_types
 from .geocrosswalk import valid_geo_shorthand, example_crosswalk_data
+from .geocrosswalk import prepare_data_product, generate_data_product
+from .geocrosswalk import SORT_PARAMS, SORT_BYS
+from .geocrosswalk import ID_COLS, CSV, ZIP, W, R
 
 from .variable_codes import code_desc_1990, desc_code_1990
 from .variable_codes import code_desc_2000_SF1b, desc_code_2000_SF1b
