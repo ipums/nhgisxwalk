79be5914116399bc95e331a6721295e8658102e3
James Gaboardi <jgaboardi@gmail.com>
2020-10-02 10:38:02 -0400

    add header to package files

--- nhgisxwalk/__init__.py
+++ nhgisxwalk/__init__.py
@@ -1,7 +1,12 @@
 __version__ = "0.0.9post1"
 """
-:mod:`nhgisxwalk` --- IPUMS/NHGIS Census Crosswalk and Atom Generator
-=====================================================================
+# `nhgisxwalk` --- IPUMS/NHGIS Census Crosswalk and Atom Generator
+
+# This file is part of the Minnesota Population Center's NHGISXWALK.
+# For copyright and licensing information, see the NOTICE and LICENSE files
+# in this project's top-level directory, and also on-line at:
+#   https://github.com/ipums/nhgisxwalk
+
 """
 
 __author__ = "James Gaboardi <jgaboardi@gmail.com>"
