21680a86b67b7ac9e19b4c53c06a57dcc025b1e0
James Gaboardi <jgaboardi@gmail.com>
2021-12-23 14:51:18 -0500

    add 3.10 to CI testing support

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ def setup_package():
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
         ],
         license="MPL-2.0",
         packages=[package],
